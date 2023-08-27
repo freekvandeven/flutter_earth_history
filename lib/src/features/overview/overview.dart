7156dadebe42167d9535e7d06febb285ce3376b1
Freek van de Ven <freek@freekvandeven.nl>
2023-08-27 12:08:01 +0200

    refactor: menu structure

--- lib/src/features/overview/overview.dart
+++ lib/src/features/overview/overview.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_earth_history/src/features/overview/widgets/menu.dart';
-import 'package:flutter_earth_history/src/global/extensions/localization.dart';
 import 'package:flutter_earth_history/src/routes.dart';
-import 'package:flutter_earth_history/src/services/application_config.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -13,54 +11,15 @@ class OverviewScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var menuOpened = useState(false);
-    var localization = context.localizations;
-    var size = MediaQuery.of(context).size;
+    // var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
         alignment: Alignment.center,
         children: [
           if (menuOpened.value) ...[
-            // semi transparant background behind the menu
-            Container(
-              color: Colors.black.withOpacity(0.3),
-              width: size.width,
-              height: size.height,
-            ),
             const MenuWidget(),
-            Positioned(
-              top: MediaQuery.of(context).padding.top + 8,
-              right: MediaQuery.of(context).padding.right + 8,
-              child: GestureDetector(
-                onTap: () async =>
-                    // change the language of the app
-                    ref
-                        .read(applicationConfigProvider.notifier)
-                        .saveApplicationSettings(
-                          ref.read(applicationConfigProvider).copyWith(
-                                language: ref
-                                            .read(applicationConfigProvider)
-                                            .language ==
-                                        'en'
-                                    ? 'nl'
-                                    : 'en',
-                              ),
-                        ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  padding: const EdgeInsets.all(8),
-                  child: Text(
-                    localization.languageSwitch,
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
-          ],
-          if (!menuOpened.value) ...[
+          ] else ...[
             // chevron down button to open the period page
             Positioned(
               bottom: MediaQuery.of(context).padding.bottom + 8,
--- lib/src/features/overview/widgets/menu.dart
+++ lib/src/features/overview/widgets/menu.dart
@@ -2,92 +2,138 @@ import 'package:flutter/material.dart';
 import 'package:flutter_earth_history/src/features/overview/widgets/about.dart';
 import 'package:flutter_earth_history/src/global/extensions/localization.dart';
 import 'package:flutter_earth_history/src/routes.dart';
+import 'package:flutter_earth_history/src/services/application_config.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MenuWidget extends StatelessWidget {
+class MenuWidget extends ConsumerWidget {
   const MenuWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var localization = context.localizations;
     var size = MediaQuery.of(context).size;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+    return Stack(
       children: [
-        const Spacer(),
-        SizedBox(
-          height: size.width * 0.6,
-          width: size.width * 0.6,
-          child: const Placeholder(),
+        // semi transparant background behind the menu
+        Container(
+          color: Colors.black.withOpacity(0.3),
+          width: size.width,
+          height: size.height,
         ),
-        SizedBox(height: size.height * 0.05),
-        GestureDetector(
-          onTap: () async =>
-              context.push(ProjectRoute.globalTimelineScreen.route),
-          behavior: HitTestBehavior.translucent,
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // icon of timeline
-                const Icon(Icons.timeline),
-                Text(localization.menuExploreTimeline),
-              ],
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(),
+            SizedBox(
+              height: size.width * 0.6,
+              width: size.width * 0.6,
+              child: const Placeholder(),
             ),
-          ),
-        ),
-        // divider line
-        Divider(
-          height: 1.0,
-          color: Colors.black,
-          indent: size.width * 0.15,
-          endIndent: size.width * 0.15,
-        ),
+            SizedBox(height: size.height * 0.05),
+            GestureDetector(
+              onTap: () async =>
+                  context.push(ProjectRoute.globalTimelineScreen.route),
+              behavior: HitTestBehavior.translucent,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // icon of timeline
+                    const Icon(Icons.timeline),
+                    Text(localization.menuExploreTimeline),
+                  ],
+                ),
+              ),
+            ),
+            // divider line
+            Divider(
+              height: 1.0,
+              color: Colors.black,
+              indent: size.width * 0.15,
+              endIndent: size.width * 0.15,
+            ),
 
-        GestureDetector(
-          onTap: () async => context.push(ProjectRoute.collectionScreen.route),
-          behavior: HitTestBehavior.translucent,
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // icon of blocks
-                const Icon(Icons.grid_view),
-                Text(localization.menuExploreCollection),
-              ],
+            GestureDetector(
+              onTap: () async =>
+                  context.push(ProjectRoute.collectionScreen.route),
+              behavior: HitTestBehavior.translucent,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // icon of blocks
+                    const Icon(Icons.grid_view),
+                    Text(localization.menuExploreCollection),
+                  ],
+                ),
+              ),
+            ),
+            // divider line
+            Divider(
+              height: 1.0,
+              color: Colors.black,
+              indent: size.width * 0.15,
+              endIndent: size.width * 0.15,
+            ),
+            GestureDetector(
+              // show the app info dialog
+              onTap: () async => showDialog(
+                // the dialog should be a small as possible
+                context: context,
+                builder: (_) => const AboutAppDialog(),
+              ),
+              behavior: HitTestBehavior.translucent,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // info icon
+                    const Icon(Icons.info_outline),
+                    Text(localization.menuExploreAbout),
+                  ],
+                ),
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+        Positioned(
+          top: MediaQuery.of(context).padding.top + 8,
+          right: MediaQuery.of(context).padding.right + 8,
+          child: GestureDetector(
+            onTap: () async =>
+                // change the language of the app
+                ref
+                    .read(applicationConfigProvider.notifier)
+                    .saveApplicationSettings(
+                      ref.read(applicationConfigProvider).copyWith(
+                            language:
+                                ref.read(applicationConfigProvider).language ==
+                                        'en'
+                                    ? 'nl'
+                                    : 'en',
+                          ),
+                    ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                localization.languageSwitch,
+                style: const TextStyle(color: Colors.white),
+              ),
             ),
           ),
         ),
-        // divider line
-        Divider(
-          height: 1.0,
-          color: Colors.black,
-          indent: size.width * 0.15,
-          endIndent: size.width * 0.15,
-        ),
-        GestureDetector(
-          // show the app info dialog
-          onTap: () async => showDialog(
-            // the dialog should be a small as possible
-            context: context,
-            builder: (_) => const AboutAppDialog(),
-          ),
-          behavior: HitTestBehavior.translucent,
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // info icon
-                const Icon(Icons.info_outline),
-                Text(localization.menuExploreAbout),
-              ],
-            ),
-          ),
-        ),
-        const Spacer(),
       ],
     );
   }
